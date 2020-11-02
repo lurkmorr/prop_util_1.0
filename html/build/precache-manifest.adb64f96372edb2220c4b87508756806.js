acd35cce2c83f7f9d43d3010a7f8a79f41c7dba6
your name <lurkmorr@gmail.com>
2020-11-02 11:13:36 +0200

    v1

---
+++ html/build/precache-manifest.adb64f96372edb2220c4b87508756806.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d53980c34bcdba7723687d6dafbe2e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba5429ea980ab3c3d9db",
+    "url": "./static/css/main.8b64ea18.chunk.css"
+  },
+  {
+    "revision": "52b6b8e9ac2bdc56bd1b",
+    "url": "./static/js/2.8e9da9b9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8e9da9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c23374be0f06286b5c4",
+    "url": "./static/js/3.dec34d59.chunk.js"
+  },
+  {
+    "revision": "ba5429ea980ab3c3d9db",
+    "url": "./static/js/main.e2dbcfaa.chunk.js"
+  },
+  {
+    "revision": "69f2eb34019c0bbcc570",
+    "url": "./static/js/runtime-main.ad38cb7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
